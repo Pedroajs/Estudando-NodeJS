af95f93d46b63607851d6495b86b5046d507cc56
Pedro Augusto <pedroajs93@gmail.com>
2020-07-25 17:54:22 -0300

    Adicionados novos metodos busca, atualização e remoção

--- src/app/infra/livro-dao.js
+++ src/app/infra/livro-dao.js
@@ -39,6 +39,64 @@ class LivroDao{
             )
         })
     }
+
+    buscaPorId(id){
+        return new Promise((resolve, reject) => {
+            this._db.get(
+                `
+                SELECT *
+                FROM livros
+                WHERE id = ?
+                `,
+                [id],
+                (erro, livro) =>{
+                    if (erro) return ('Não foi possível buscar livro pelo id');
+                    return resolve(livro);
+                }
+            )
+        })
+    }
+
+    atualiza(livro){
+        return new Promise((resolve, reject) =>{
+            this._db.run(
+                `
+                UPDATE livros SET
+                titulo = ?,
+                preco = ?,
+                descricao = ?,
+                WHERE id = ?
+                `,
+                [
+                    livro.titulo,
+                    livro.preco,
+                    livro.descricao,
+                    livro.id
+                ],
+                erro =>{
+                    if (erro) return reject ("Não foi possível atualizar o livro");
+                    resolve();
+                }
+            )
+        })
+    }
+
+    remove(id){
+        return new Promise((resolve, reject)=>{
+            this._db.run(
+                `
+                DELETE *
+                FROM livros
+                WHERE id = ?
+                `,
+                [id],
+                erro =>{
+                    if(erro) return reject('Não foi possível exluir livro');
+                    return resolve();
+                }
+            )
+        })
+    }
 }
 module.exports = LivroDao;
 
