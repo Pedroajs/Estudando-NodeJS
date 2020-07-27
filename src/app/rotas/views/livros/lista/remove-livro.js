ca5a75693f080e5551a60b2dbdd4e2a61feb6386
Pedro Augusto <pedroajs93@gmail.com>
2020-07-27 13:54:36 -0300

    script para remoção de livros feitos pro lado do navegador

---
+++ src/app/rotas/views/livros/lista/remove-livro.js
@@ -0,0 +1,17 @@
+const tabelaLivro = document.querySelector('#livros');
+
+tabelaLivro.addEventListener('click', evento =>{
+    let elementoClicado = evento.target;
+
+    if(elementoClicado.dataset.type == 'remocao'){
+        let livroId = elementoClicado.dataset.ref;
+        fetch(`http://localhost:3000/livros/${livroId}`, {method: 'DELETE'})
+            .then(resposta =>{
+                let tr = elementoClicado.closest(`#livro_${livroId}`);
+                tr.remove();
+            })
+            .catch(erro => console.log(erro));
+    }
+
+
+})
