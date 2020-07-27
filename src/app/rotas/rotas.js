f6d07c5262a04a2ac86fcbe343ea47abe7dfa087
Pedro Augusto <pedroajs93@gmail.com>
2020-07-27 14:59:34 -0300

    add pasta public com arquivos js

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -44,6 +44,16 @@ module.exports = (app) =>{
                 .catch(erro => console.log(erro));
         
     } );
+
+    app.delete('/livros/:id', (req,resp) => {
+        const id = req.params.id;
+        const livroDao = new LivroDao(db);
+
+        livroDao.remove(id)
+                .then(resp.status(200).end())
+                .catch(erro =>console.log(erro))
+                
+    })
     
 }
 
